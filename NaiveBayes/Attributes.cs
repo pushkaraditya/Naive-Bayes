72c5243a2b5114479d28866b0384c1f47d4a6053
Pushkar Aditya <pushkar.aditya@askiitians.com>
2015-10-30 11:54:13 +0530

    attribute constraints

--- NaiveBayes/Attributes.cs
+++ NaiveBayes/Attributes.cs
@@ -2,6 +2,9 @@
 
 namespace NaiveBayes
 {
+    [AttributeUsage(AttributeTargets.Property)]
     public class FactAttribute : Attribute { }
+    
+    [AttributeUsage(AttributeTargets.Property)]
     public class ClassAttribute : Attribute { }
 }
